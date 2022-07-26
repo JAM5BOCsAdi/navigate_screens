7dd0116e762627189ea0a1be5191a79e04158503
CsAdi <oradam86@gmail.com>
2022-07-26 19:40:02 +0200

    Navigate Between Screens

--- lib/screen0.dart
+++ lib/screen0.dart
@@ -13,6 +13,7 @@ class Screen0 extends StatelessWidget {
         ),
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               ElevatedButton(
                 style: ButtonStyle(
